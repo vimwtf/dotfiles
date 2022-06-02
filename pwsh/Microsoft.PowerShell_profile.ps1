47b9484395d23f3b535104101722d50553d8ed6c
John Bowdre <john@bowdre.net>
2022-06-02 08:21:51 -0500

    pwsh: add tanzu and kubectl aliases

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,4 @@
+New-Alias t tanzu
+New-Alias k kubectl
 oh-my-posh init pwsh --config https://raw.githubusercontent.com/jbowdre/dotfiles/main/oh-my-posh/vpotato.minimal.omp.json | Invoke-Expression
 Enable-PoshTransientPrompt
