9a39b4b7c5d1e383b490e389292299f17637e2ed
John Bowdre <john@bowdre.net>
2023-01-20 09:29:34 -0600

    pwsh: remove tanzu alias

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,3 @@
-New-Alias t tanzu
 New-Alias k kubectl
 oh-my-posh init pwsh --config https://raw.githubusercontent.com/jbowdre/dotfiles/main/oh-my-posh/vpotato.minimal.omp.json | Invoke-Expression
 Enable-PoshTransientPrompt
