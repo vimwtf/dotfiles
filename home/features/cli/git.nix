34d3e9bcc206d52e66caf78fc09afec360f252e6
John Bowdre <john@bowdre.net>
2023-08-04 10:34:11 -0500

    fix git config

--- home/features/cli/git.nix
+++ home/features/cli/git.nix
@@ -1,16 +1,20 @@
-{ lib, ... }: {
-  programs.git = {
-    enable = lib.mkDefault true;
-    delta.enable = true;
-    aliases = {
-      graph = "log --decorate --oneline --graph";
-      fast-forward = "merge --ff-only";
+{ lib, ... }:
+  let
+    email = lib.concatStringsSep "" [ "john@bo" "wdre.net" ];
+  in
+  {
+    programs.git = {
+      enable = lib.mkDefault true;
+      delta.enable = true;
+      aliases = {
+        graph = "log --decorate --oneline --graph";
+        fast-forward = "merge --ff-only";
+      };
+      userEmail = lib.mkDefault email;
+      userName = lib.mkDefault "John Bowdre";
+      extraConfig = {
+        init.defaultBranch = "main";
+        pull.rebase = false;
+      };
     };
-    userEmail = lib.concatStringsSep "" ["john@bo" "wdre.net"];
-    userName = lib.mkDefault "John Bowdre";
-    extraConfig = {
-      init.defaultBranch = "main";
-      pull.rebase = false;
-    };
-  };
-}
+  }
