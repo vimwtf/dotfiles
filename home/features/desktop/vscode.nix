ab178f06515f35db7270425e8050441f19bd4408
John Bowdre <john@bowdre.net>
2023-08-18 11:13:01 -0500

    add extensions from vscode marketplace

--- home/features/desktop/vscode.nix
+++ home/features/desktop/vscode.nix
@@ -6,6 +6,7 @@
     enableUpdateCheck = false;
     package = pkgs.unstable.vscode;
     extensions = with pkgs.vscode-extensions; [
+      # extensions available as nix packages
       bbenoist.nix
       github.copilot
       golang.go
@@ -17,6 +18,45 @@
       timonwong.shellcheck
       vscodevim.vim
       yzhang.markdown-all-in-one
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      # extensions from the vscode marketplace
+      {
+        name = "auto-align";
+        publisher = "bladnman";
+        version = "0.0.13";
+        sha256 = "sha256-N5XMHW6UadKjE29p2vevtjgqLA4QysYu2xT3njmyHLk=";
+      }
+      # copilot chat requires vscode 1.82
+      # {
+      #   name = "copilot-chat";
+      #   publisher = "github";
+      #   version = "0.7.2023081801";
+      #   sha256 = "sha256-4n28k0pqHEluL7mbsTfMMkePfRK5U5MzMNhLTRogoVY=";
+      # }
+      {
+        name = "hadolint";
+        publisher = "exiasr";
+        version = "1.1.2";
+        sha256 = "sha256-6GO1f8SP4CE8yYl87/tm60FdGHqHsJA4c2B6UKVdpgM=";
+      }
+      {
+        name = "hcl";
+        publisher = "hashicorp";
+        version = "0.3.2";
+        sha256 = "sha256-cxF3knYY29PvT3rkRS8SGxMn9vzt56wwBXpk2PqO0mo=";
+      }
+      {
+        name = "jinjahtml";
+        publisher = "samuelcolvin";
+        version = "0.20.0";
+        sha256 = "sha256-wADL3AkLfT2N9io8h6XYgceKyltJCz5ZHZhB14ipqpM=";
+      }
+      {
+        name = "markdown-table-prettify";
+        publisher = "darkriszty";
+        version = "3.6.0";
+        sha256 = "sha256-FZTiNGSY+8xk3DJsTKQu4AHy1UFvg0gbrzPpjqRlECI=";
+      }
     ];
     keybindings = [
       {
