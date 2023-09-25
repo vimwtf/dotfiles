9794125f0a2789d38e6486cf31530e0d5acf6bdf
John Bowdre <john@bowdre.net>
2023-09-25 08:52:35 -0500

    gnome: attempt to prevent device from sleeping

--- hosts/common/optional/gnome.nix
+++ hosts/common/optional/gnome.nix
@@ -1,13 +1,35 @@
 {
-  services.xserver = {
-    enable = true;
-    displayManager = {
-      gdm.enable = true;
+  services = {
+    xserver = {
+      enable = true;
+      displayManager = {
+        gdm = {
+          enable = true;
+          autoSuspend = false;
+      };
+      desktopManager = {
+        gnome.enable = true;
+      };
+      layout = "us";
+      xkbVariant = "";
     };
-    desktopManager = {
-      gnome.enable = true;
-    };
-    layout = "us";
-    xkbVariant = "";
+    logind.lidSwitch = "ignore";
+  };
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+        if (action.id == "org.freedesktop.login1.suspend" ||
+            action.id == "org.freedesktop.login1.suspend-multiple-sessions" ||
+            action.id == "org.freedesktop.login1.hibernate" ||
+            action.id == "org.freedesktop.login1.hibernate-multiple-sessions")
+        {
+            return polkit.Result.NO;
+        }
+    });
+  '';
+  systemd.targets = {
+    sleep.enable = false;
+    suspend.enable = false;
+    hibernate.enable = false;
+    hybrid-sleep.enable = false;
   };
 }
