b582f48ae05c8e256f88360229a1abb453dd0105
John Bowdre <john@bowdre.net>
2024-02-10 22:00:23 -0600

    fish: set insert mode cursor to blinking block

--- home/features/cli/fish.nix
+++ home/features/cli/fish.nix
@@ -47,8 +47,8 @@
       # Use vim bindings and cursors
       ''
         fish_vi_key_bindings
-        set fish_cursor_default       block       blink
-        set fish_cursor_insert        line        blink
+        set fish_cursor_default       block
+        set fish_cursor_insert        block       blink
         set fish_cursor_replace_one   underscore  blink
         set fish_cursor_visual        block
       '' +
