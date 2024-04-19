1d507c6768c321b4abb09d6ca64d2018c9c4a9f0
John Bowdre <john@bowdre.net>
2024-04-19 15:57:30 -0500

    standardize on 23.11 release

--- flake.nix
+++ flake.nix
@@ -6,15 +6,15 @@
   # - https://github.com/Misterio77/nix-config/
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's a working example:
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
-    # Also see the 'stable-packages' overlay at 'nix/overlays/default.nix'.
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Also see the 'unstable-packages' overlay at 'nix/overlays/default.nix'.
 
     # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
