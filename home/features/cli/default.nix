3d0c9dcae47cc541fbd82e9ae8b96a913067076e
John Bowdre <john@bowdre.net>
2024-09-11 20:26:12 -0500

    cli: add pigz

--- home/features/cli/default.nix
+++ home/features/cli/default.nix
@@ -27,6 +27,7 @@
     nmap # Network scanner
     nodejs # Node js
     nodePackages.npm # Node package manager
+    pigz # Multithreaded gzip
     powershell # Powershell
     pssh # Parallel SSH
     python3 # Python 3
