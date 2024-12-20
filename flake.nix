dc797f4b6a4fd6f111fc76490f569310d215eaea
John Bowdre <john@bowdre.net>
2024-12-19 19:00:00 -0600

    track nix 24.11

--- flake.nix
+++ flake.nix
@@ -6,15 +6,15 @@
   # - https://github.com/Misterio77/nix-config/
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's a working example:
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
-    # Also see the 'stable-packages' overlay at 'nix/overlays/default.nix'.
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -29,7 +29,7 @@
 
     # nixvim
     nixvim = {
-      url = "github:nix-community/nixvim";
+      url = "github:nix-community/nixvim/nixos-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
--- overlays/default.nix
+++ overlays/default.nix
@@ -15,8 +15,8 @@
 
   # When applied, the stable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.stable'
-  stable-packages = final: _prev: {
-    stable = import inputs.nixpkgs-stable {
+  unstable-packages = final: _prev: {
+    unstable = import inputs.nixpkgs-unstable {
       system = final.system;
       config.allowUnfree = true;
     };
