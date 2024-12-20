6b7a5b445df169de49475b921a8d1592348167cb
John Bowdre <john@bowdre.net>
2024-12-19 19:07:08 -0600

    home-manager: track 24.11

--- home/global/default.nix
+++ home/global/default.nix
@@ -22,7 +22,7 @@
   home = {
     username = lib.mkDefault "john";
     homeDirectory = lib.mkDefault "/home/${config.home.username}";
-    stateVersion = lib.mkDefault "23.05";
+    stateVersion = lib.mkDefault "24.11";
 
     sessionVariables = {
       EDITOR = "nvim";
