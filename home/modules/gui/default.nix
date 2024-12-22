a9dbd699cb1030239c983691c667d0d3a7814c6f
John Bowdre <john@bowdre.net>
2024-12-22 08:26:11 -0600

    remove vscode

--- home/modules/gui/default.nix
+++ home/modules/gui/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   # Common GUI apps
-  imports = [ ./firefox.nix ./kitty.nix ./vscode.nix ];
+  imports = [ ./firefox.nix ./kitty.nix ];
 
   home.packages = with pkgs; [ gimp-with-plugins qFlipper remmina ];
 }
