12d74b630bd0c8f14a133ccef2e45e2098a1f277
John Bowdre <john@bowdre.net>
2024-12-22 08:21:08 -0600

    kitty: remember window size

--- home/modules/gui/kitty.nix
+++ home/modules/gui/kitty.nix
@@ -9,6 +9,7 @@
       enable_audio_bell = "no";
       linux_display_server = "x11";
       paste_actions = "quote-urls-at-prompt,confirm,confirm-if-large";
+      remember_window_size = "yes";
       resize_debounce_time = "0.5";
       shell = "${pkgs.fish}/bin/fish";
       strip_trailing_spaces = "smart";
