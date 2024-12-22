7767c21df5a618a415143b517788e61efc018ad5
John Bowdre <john@bowdre.net>
2024-12-21 19:29:05 -0600

    kitty: config++

--- home/modules/gui/kitty.nix
+++ home/modules/gui/kitty.nix
@@ -2,8 +2,20 @@
 
   programs.kitty = {
     enable = true;
-    # package = (config.lib.nixGL.wrap pkgs.kitty);
-    # settings = { linux_display_server = "x11"; };
+    font.name = "BerkeleyMono Nerd Font Mono";
+    package = (config.lib.nixGL.wrap pkgs.kitty);
+    settings = {
+      editor = "${pkgs.neovim}/bin/nvim";
+      enable_audio_bell = "no";
+      linux_display_server = "x11";
+      paste_actions = "quote-urls-at-prompt,confirm,confirm-if-large";
+      resize_debounce_time = "0.5";
+      shell = "${pkgs.fish}/bin/fish";
+      strip_trailing_spaces = "smart";
+      visual_bell_duration = "1.0 ease-in-out";
+    };
+    shellIntegration.enableFishIntegration = true;
+    themeFile = "tokyo_night_night";
   };
 }
 
