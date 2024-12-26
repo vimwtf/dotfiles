d58c8082088da6d31bb9baadbac21180c277ab24
John Bowdre <john@bowdre.net>
2024-12-26 17:18:34 -0600

    kitty: unlink from default

--- home/modules/gui/default.nix
+++ home/modules/gui/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   # Common GUI apps
-  imports = [ ./firefox.nix ./ghostty.nix ./kitty.nix ];
+  imports = [ ./firefox.nix ./ghostty.nix ];
 
   home.packages = with pkgs; [ gimp-with-plugins qFlipper remmina ];
 }
