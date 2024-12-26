9ca305b0158f35fc7a5edf2ca61a9a581f68684e
John Bowdre <john@bowdre.net>
2024-12-26 17:17:50 -0600

    ghostty: add config

--- home/modules/gui/ghostty.nix
+++ home/modules/gui/ghostty.nix
@@ -1,7 +1,17 @@
-{ config, inputs, ... }: {
+{ config, inputs, pkgs, ... }: {
 
   home.packages =
     [ (config.lib.nixGL.wrap inputs.ghostty.packages.x86_64-linux.default) ];
 
+  xdg.configFile."ghostty/config".text = ''
+    auto-update = off
+    command = ${pkgs.fish}/bin/fish
+    font-family = BerkeleyMono Nerd Font Mono
+    initial-command = ${pkgs.fish}/bin/fish -c "${pkgs.tmux}/bin/tmux attach-session -t (hostname -s) || tmux new-session -s (hostname -s) -c $HOME"
+    shell-integration = fish
+    theme = catppuccin-mocha
+    window-height = 50
+    window-width = 170
+  '';
 }
 
