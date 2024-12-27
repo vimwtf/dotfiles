329637ce7405feaa8084517d6cbc12813384c604
John Bowdre <john@bowdre.net>
2024-12-26 18:55:55 -0600

    ghostty: don't auto-launch tmux

--- home/modules/gui/ghostty.nix
+++ home/modules/gui/ghostty.nix
@@ -7,7 +7,6 @@
     auto-update = off
     command = ${pkgs.fish}/bin/fish
     font-family = BerkeleyMono Nerd Font Mono
-    initial-command = ${pkgs.fish}/bin/fish -c "${pkgs.tmux}/bin/tmux attach-session -t (hostname -s) || tmux new-session -s (hostname -s) -c $HOME"
     shell-integration = fish
     theme = catppuccin-mocha
     window-height = 50
