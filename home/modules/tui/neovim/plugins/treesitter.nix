3f043328befb20efd21eef675bb360ddfd193175
John Bowdre <john@bowdre.net>
2025-01-02 17:14:21 -0600

    neovim: increment/decrement node selection with v/V

--- home/modules/tui/neovim/plugins/treesitter.nix
+++ home/modules/tui/neovim/plugins/treesitter.nix
@@ -8,6 +8,11 @@
       settings = {
         highlight.enable = true;
         indent.enable = true;
+        incremental_selection = {
+          enable = true;
+          keymaps.node_incremental = "v";
+          keymaps.node_decremental = "V";
+        };
       };
       folding = true;
     };
