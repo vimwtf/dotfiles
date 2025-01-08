c9c8b576fadc98420af443f72760ccee34f63a8b
John Bowdre <john@bowdre.net>
2025-01-08 11:33:58 -0600

    neovim: md-preview behavior tweaks

--- home/modules/tui/neovim/plugins/md-preview.nix
+++ home/modules/tui/neovim/plugins/md-preview.nix
@@ -5,14 +5,23 @@
 
       settings = {
         auto_close = 0;
+        combine_preview = 1;
+        echo_preview_url = 1;
         theme = "dark";
       };
     };
 
-    files."after/ftplugin/markdown.lua".keymaps = [{
-      mode = "n";
-      key = "<leader>m";
-      action = ":MarkdownPreview<cr>";
-    }];
+    files."after/ftplugin/markdown.lua".keymaps = [
+      {
+        mode = "n";
+        key = "<leader>m";
+        action = ":MarkdownPreview<cr>";
+      }
+      {
+        mode = "n";
+        key = "<leader>M";
+        action = ":MarkdownPreviewStop<cr>";
+      }
+    ];
   };
 }
