f46f31d705c6a8e677c9cdb3cf102de7f242b3c7
John Bowdre <john@bowdre.net>
2025-01-08 09:12:25 -0600

    fix comments for unstable overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -13,8 +13,8 @@
       # });
     };
 
-  # When applied, the stable nixpkgs set (declared in the flake inputs) will
-  # be accessible through 'pkgs.stable'
+  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
+  # be accessible through 'pkgs.unstable'
   unstable-packages = final: _prev: {
     unstable = import inputs.nixpkgs-unstable {
       system = final.system;
