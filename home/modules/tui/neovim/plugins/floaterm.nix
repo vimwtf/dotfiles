18a01568dc67ac38ce343798781476088c58525e
John Wq <wq@vim.wtf>
2025-09-19 11:34:01 -0500

    fix: correct configuration for floaterm nvim plugin

--- home/modules/tui/neovim/plugins/floaterm.nix
+++ home/modules/tui/neovim/plugins/floaterm.nix
@@ -1,11 +1,11 @@
 {
   programs.nixvim.plugins.floaterm = {
+    enable = true;
     settings = {
-      enable = true;
       width = 0.8;
       height = 0.8;
       title = "";
-      keymaps_toggle = "<leader>,";
+      keymap_toggle = "<leader>,";
     };
   };
 }
