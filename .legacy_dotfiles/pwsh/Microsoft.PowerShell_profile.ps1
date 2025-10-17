401abb3c923737063f3a9c9c13ac50648973447e
John Wq <wq@vim.wtf>
2025-10-16 19:54:54 -0500

    remove legacy dotfiles

--- .legacy_dotfiles/pwsh/Microsoft.PowerShell_profile.ps1
+++
@@ -1,3 +0,0 @@
-New-Alias k kubectl
-oh-my-posh init pwsh --config https://raw.githubusercontent.com/jbowdre/dotfiles/main/oh-my-posh/vpotato.minimal.omp.json | Invoke-Expression
-Enable-PoshTransientPrompt
