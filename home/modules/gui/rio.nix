3bdce9536bea7650bcd9184dada61912e9e91b63
John Wq <wq@vim.wtf>
2025-10-20 11:08:18 -0500

    rio: additional configuration

--- home/modules/gui/rio.nix
+++ home/modules/gui/rio.nix
@@ -5,9 +5,17 @@
     enable = true;
     package = with pkgs; (config.lib.nixGL.wrap rio);
     settings = {
+      bell = {
+        audio = false;
+        visual = true;
+      };
+      cursor.blinking = true;
       fonts.family = "VictorMono Nerd Font Mono";
       shell.program = "${pkgs.fish}/bin/fish";
-      window.mode = "Maximized";
+      window = {
+        decorations = "Buttonless";
+        mode = "Maximized";
+      };
     };
   };
 }
