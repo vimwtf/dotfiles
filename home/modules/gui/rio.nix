ef1253dec94823252b88fd129e26ab9042c2d1a8
John Wq <wq@vim.wtf>
2025-10-20 09:37:20 -0500

    rio: wrap with nixgl, set shell to fish

--- home/modules/gui/rio.nix
+++ home/modules/gui/rio.nix
@@ -1,9 +1,12 @@
-{ ... }:
+{ config, pkgs, ... }:
 {
+
   programs.rio = {
     enable = true;
+    package = with pkgs; (config.lib.nixGL.wrap rio);
     settings = {
       fonts.family = "VictorMono Nerd Font Mono";
+      shell.program = "${pkgs.fish}/bin/fish";
       window.mode = "Maximized";
     };
   };
