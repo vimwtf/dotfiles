6332c5eefa7a43bedfbcc03398a8d572465c50fe
John Wq <wq@vim.wtf>
2025-10-20 09:37:51 -0500

    chore: compatibility updates

--- home/modules/tui/git/default.nix
+++ home/modules/tui/git/default.nix
@@ -11,24 +11,29 @@
 
   home.packages = lib.mkIf isWork [ pkgs.git-credential-manager ];
 
-  programs.git = {
-    enable = lib.mkDefault true;
-    delta.enable = true;
-    aliases = {
-      graph = "log --decorate --oneline --graph";
-      fast-forward = "merge --ff-only";
+  programs = {
+    delta = {
+      enable = true;
+      enableGitIntegration = true;
     };
-    extraConfig = lib.mkMerge [
-      {
-        gpg.format = "ssh";
-        init.defaultBranch = "main";
-        pull.rebase = false;
-        push.autoSetupRemote = true;
-        user.signingKey = "~/.ssh/id_ed25519.pub";
-      }
-      (lib.mkIf isWork { credential.credentialStore = "gpg"; })
-    ];
-    includes = [ { path = "${config.sops.secrets.git-config.path}"; } ];
+    git = {
+      enable = lib.mkDefault true;
+      settings = lib.mkMerge [
+        {
+          aliases = {
+            graph = "log --decorate --oneline --graph";
+            fast-forward = "merge --ff-only";
+          };
+          gpg.format = "ssh";
+          init.defaultBranch = "main";
+          pull.rebase = false;
+          push.autoSetupRemote = true;
+          user.signingKey = "~/.ssh/id_ed25519.pub";
+        }
+        (lib.mkIf isWork { credential.credentialStore = "gpg"; })
+      ];
+      includes = [ { path = "${config.sops.secrets.git-config.path}"; } ];
+    };
   };
 
   programs.fish = lib.mkMerge [
