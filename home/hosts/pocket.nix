b20517d06fa7cf170d00f654988e416527a614b1
John Wq <wq@vim.wtf>
2025-10-30 11:39:44 -0500

    pocket: configure foot

--- home/hosts/pocket.nix
+++ home/hosts/pocket.nix
@@ -10,4 +10,17 @@
   ];
 
   fonts.fontconfig.enable = true;
+
+  programs.foot = {
+    enable = true;
+    package = with pkgs; foot;
+
+    settings = {
+      main = {
+        font = "VictorMono Nerd Font Mono:size=11";
+        shell = "${pkgs.fish}/bin/fish";
+        dpi-aware = "no";
+      };
+    };
+  };
 }
