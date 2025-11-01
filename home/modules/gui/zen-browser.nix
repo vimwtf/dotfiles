7f62f91e9ddba1175c93c81fcd1d4e25f533d010
John Wq <wq@vim.wtf>
2025-11-01 13:37:49 -0500

    add initial zen browser config

---
+++ home/modules/gui/zen-browser.nix
@@ -0,0 +1,131 @@
+{ config, inputs, ... }:
+{
+  imports = [
+    inputs.zen-browser.homeModules.beta
+  ];
+
+  programs.zen-browser = {
+    enable = true;
+    policies =
+      let
+        mkLockedAttrs = builtins.mapAttrs (
+          _: value: {
+            Value = value;
+            Status = "locked";
+          }
+        );
+
+        mkPluginUrl = id: "https://addons.mozilla.org/firefox/downloads/latest/${id}/latest.xpi";
+
+        mkExtensionEntry =
+          {
+            id,
+            pinned ? false,
+          }:
+          let
+            base = {
+              install_url = mkPluginUrl id;
+              installation_mode = "force_installed";
+            };
+          in
+          if pinned then base // { default_area = "navbar"; } else base;
+
+        mkExtensionSettings = builtins.mapAttrs (
+          _: entry: if builtins.isAttrs entry then entry else mkExtensionEntry { id = entry; }
+        );
+      in
+      {
+        AutofillAddressEnabled = false;
+        AutofillCreditCardEnabled = false;
+        DisableAppUpdate = true;
+        DisableFeedbackCommands = true;
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        DontCheckDefaultBrowser = true;
+        OfferToSaveLogins = false;
+        EnableTrackingProtection = {
+          Value = true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+        ExtensionSettings = mkExtensionSettings {
+          "uBlock0@raymondhill.net" = mkExtensionEntry {
+            id = "ublock-origin";
+            pinned = true;
+          };
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = mkExtensionEntry {
+            id = "bitwarden-password-manager";
+            pinned = true;
+          };
+        };
+        Preferences = mkLockedAttrs {
+          "browser.aboutConfig.showWarning" = false;
+          "browser.tabs.warnOnClose" = false;
+          "media.videocontrols.picture-in-picture.video-toggle.enabled" = true;
+          # Disable swipe gestures (Browser:BackOrBackDuplicate, Browser:ForwardOrForwardDuplicate)
+          "browser.gesture.swipe.left" = "";
+          "browser.gesture.swipe.right" = "";
+          "browser.tabs.hoverPreview.enabled" = true;
+          "browser.newtabpage.activity-stream.feeds.topsites" = false;
+          "browser.topsites.contile.enabled" = false;
+
+          "privacy.resistFingerprinting" = true;
+          "privacy.firstparty.isolate" = true;
+          "network.cookie.cookieBehavior" = 5;
+          "dom.battery.enabled" = false;
+
+          "gfx.webrender.all" = true;
+          "network.http.http3.enabled" = true;
+        };
+      };
+
+    profiles.default = rec {
+      settings = {
+        "zen.workspaces.continue-where-left-off" = false;
+        "zen.workspaces.natural-scroll" = true;
+        "zen.view.compact.hide-tabbar" = true;
+        "zen.view.compact.hide-toolbar" = true;
+        "zen.view.compact.animate-sidebar" = false;
+        "zen.welcome-screen.seen" = true;
+      };
+    };
+  };
+
+  xdg.mimeApps =
+    let
+      associations = builtins.listToAttrs (
+        map
+          (name: {
+            inherit name;
+            value =
+              let
+                zen-browser = config.programs.zen-browser.package;
+              in
+              zen-browser.meta.desktopFileName;
+          })
+          [
+            "application/x-extension-shtml"
+            "application/x-extension-xhtml"
+            "application/x-extension-html"
+            "application/x-extension-xht"
+            "application/x-extension-htm"
+            "x-scheme-handler/unknown"
+            "x-scheme-handler/mailto"
+            "x-scheme-handler/chrome"
+            "x-scheme-handler/about"
+            "x-scheme-handler/https"
+            "x-scheme-handler/http"
+            "application/xhtml+xml"
+            "application/json"
+            "text/plain"
+            "text/html"
+          ]
+      );
+    in
+    {
+      associations.added = associations;
+      defaultApplications = associations;
+    };
+}
