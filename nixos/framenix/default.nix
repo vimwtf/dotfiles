598534b45498cc0346f563ae4713dd5ba0d0429a
John Wq <wq@vim.wtf>
2025-11-08 17:06:31 -0600

    framenix: remove gnome

--- nixos/framenix/default.nix
+++ nixos/framenix/default.nix
@@ -10,7 +10,7 @@
     ../common/users/john
 
     ../common/optional/docker.nix
-    ../common/optional/gnome.nix
+    # ../common/optional/gnome.nix
     ../common/optional/libvirtd.nix
     ../common/optional/pipewire.nix
     ../common/optional/printing.nix
