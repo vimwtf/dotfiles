c0471cc350537c867373f3daaff3d5bdb6963317
John Wq <wq@vim.wtf>
2025-11-08 15:14:50 -0600

    foot: set fish as default shell

--- home/modules/gui/foot.nix
+++ home/modules/gui/foot.nix
@@ -1,10 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.foot = {
     enable = true;
     settings = {
       main = {
         font = "VictorMono Nerd Font Mono:size=10";
+        shell = "${pkgs.fish}/bin/fish";
       };
     };
   };
