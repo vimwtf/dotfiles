0dfac3d1233a0ccd3cf21bff504c56b22d2d784d
John Wq <wq@vim.wtf>
2025-11-12 14:59:20 -0600

    pocket: load reform-tray at sway startup

--- home/hosts/pocket.nix
+++ home/hosts/pocket.nix
@@ -6,6 +6,13 @@
     ../modules/gui/sway
   ];
 
+  wayland.windowManager.sway.config.startup = [
+    {
+      command = "/usr/bin/python3 /usr/libexec/reform-tools/reform-tray.py";
+      always = true;
+    }
+  ];
+
   # extra packages
   # home.packages = with pkgs; [
   #   nerd-fonts.victor-mono
