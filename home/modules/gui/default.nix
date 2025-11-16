955b162e59a17d8455d9d51cad56a25d39ae80ef
John Wq <wq@vim.wtf>
2025-11-16 13:11:40 -0600

    gui: add thunar

--- home/modules/gui/default.nix
+++ home/modules/gui/default.nix
@@ -8,6 +8,7 @@
   home.packages = with pkgs; [
     deltachat-desktop
     nerd-fonts.victor-mono
+    xfce.thunar
   ];
 
   fonts.fontconfig.enable = true;
---
+++ home/modules/gui/thunar.nix
@@ -0,0 +1,4 @@
+{ ... }:
+{
+  programs.thunar.enable = true;
+}
