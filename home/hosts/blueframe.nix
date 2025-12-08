595b6c0c2d6347cd56f8ba732d37465cf48a4127
John Wq <wq@vim.wtf>
2025-12-07 23:14:16 -0600

    don't manage gui apps on blueframe

--- home/hosts/blueframe.nix
+++ home/hosts/blueframe.nix
@@ -3,17 +3,10 @@
   imports = [
     ../global
     ../modules/tui/extras
-    # ../modules/gui
-    # ../modules/gui/chrome.nix
   ];
 
   # packages
   home.packages = with pkgs; [
-    deltachat-desktop
     ente-cli
-    nerd-fonts.victor-mono
-    tor-browser
   ];
-
-  fonts.fontconfig.enable = true;
 }
